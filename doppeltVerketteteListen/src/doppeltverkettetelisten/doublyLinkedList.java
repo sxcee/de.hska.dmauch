0267263c6fe8379672ff91d02a43e4bb3df89c53
mada1024 <mada1024@3f5b4620-a983-41a6-8991-d2e62cf16891>
2014-10-20 12:18:56 +0000

    Grundgeruest der Aufgabe Zwei der Informatik2 Uebungen

---
+++ doppeltVerketteteListen/src/doppeltverkettetelisten/doublyLinkedList.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doppeltverkettetelisten;
+
+/**
+ *
+ * @author DMauch
+ */
+public class doublyLinkedList {
+    /**
+     * Traegt das Element vorne in die Liste ein.
+     * @param value 
+     */
+    public void addFirst(String value){
+        
+    }
+    /**
+     * Haengt das Element hinten an die Liste an.
+     * @param value 
+     */
+    public void addLast(String value){
+        
+    }
+    /**
+     * Fuegt das Element am angegebenen Index in die Liste ein. Wenn der Index 
+     * der Anzahl der Elemente in der Liste entspricht, wird der Wert als am Ende
+     * angehaengt.
+     * @param index
+     * @param value 
+     */
+    public void add(int index, String value){
+        
+    }
+    /**
+     * Liest den Wert am übergebenen Index aus.
+     * @param index
+     * @return 
+     */
+    public String get(int index){
+        return "";
+    }
+    /**
+     * Loescht das erste Element.
+     * @return 
+     */
+    public String removeFirst(){
+        return "";
+    }
+    /**
+     * Loescht das letzte Element.
+     * @return 
+     */
+    public String removeLast(){
+        return "";
+    }
+    /**
+     * Loescht das Element am angegebenen Index und gibt den darin gespeicherten
+     * Wert zurueck.
+     * @param index
+     * @return 
+     */
+    public String remove(int index){
+        return "";
+    }
+    /**
+     * Liest die Anzahl der Werte in der Liste aus.
+     * @return 
+     */
+    public int getSize(){
+        return 0;
+    }
+}
