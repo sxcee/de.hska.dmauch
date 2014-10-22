4a8f51525cdc791b65f5c3b836e58f65481be4ab
mada1024 <mada1024@3f5b4620-a983-41a6-8991-d2e62cf16891>
2014-10-22 10:42:57 +0000

    Neuanlegung des Projekts LinkedLists
    beinhaltet einfach und doppelt Verkettete Listen

---
+++ LinkedLists/src/linkedlists/LinkedList.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlists;
+
+/**
+ *
+ * @author mada1024
+ */
+public class LinkedList {
+    private ListElement firstElement;
+    private ListElement lastElement;
+    private int size;
+    
+    public void addLast(){
+        
+    }
+    
+    public int getSize(){
+        return size;
+    }
+    
+    
+}
---
+++ LinkedLists/src/linkedlists/ListElement.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlists;
+
+/**
+ *
+ * @author mada1024
+ */
+public class ListElement {
+    private ListElement next;
+    private ListElement prev;
+    private String value;
+    
+    public void setNextElement(){
+        
+    }
+    public ListElement getNextElement(){
+        return next;
+    }
+    public void setPrevElement(){
+        
+    }
+    public ListElement getPrevElement(){
+        return prev;
+    }
+    public void setValue(){
+        
+    }
+    public String getValue(){
+        return value;
+    }
+}
