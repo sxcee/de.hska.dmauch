25a9737d630e0c0ab9acda80d4ca2449c7ecc53c
mada1024 <mada1024@3f5b4620-a983-41a6-8991-d2e62cf16891>
2014-10-22 20:54:25 +0000

    aufgabe eins fertig

---
+++ softwareLabor_aufgabeEins/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=softwarelabor_aufgabeeins
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/softwarelabor_aufgabeeins
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=softwarelaboraufgabeeins.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/softwarelaboraufgabeeins.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=softwarelabor_aufgabeeins
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/softwarelabor_aufgabeeins
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=softwarelaboraufgabeeins.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/softwarelaboraufgabeeins.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
