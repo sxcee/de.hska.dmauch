2896dbf352b1d5336e5097f260b85f96f9da4c0a
mada1024 <mada1024@3f5b4620-a983-41a6-8991-d2e62cf16891>
2014-10-22 11:28:47 +0000

    einige funktionen fertig add(index, value) noch nicht abgeschlossen

--- LinkedLists/src/linkedlists/LinkedList.java
+++ LinkedLists/src/linkedlists/LinkedList.java
@@ -14,7 +14,83 @@ public class LinkedList {
     private ListElement lastElement;
     private int size;
     
-    public void addLast(){
+    public ListElement getFirstElement(){
+        return firstElement;
+    }
+    
+    public void setFirstElement(ListElement newElement){
+        firstElement = newElement;
+    }
+    
+    public ListElement getLastElement(){
+        return lastElement;
+    }
+    
+    public void setLastElement(ListElement newElement){
+        lastElement = newElement;
+    }
+    
+    public void addFirst(String value){
+        if(size++ == 0){
+            ListElement newElement = new ListElement();
+            newElement.setValue(value);
+            
+            setFirstElement(newElement);
+            setLastElement(newElement);
+        } else {
+            ListElement newElement = new ListElement();
+            newElement.setValue(value);
+            
+            newElement.setNextElement(getFirstElement());
+            setFirstElement(newElement);
+        }
+    }
+    
+    public void addLast(String value){
+        if(size++ == 0){
+            ListElement newElement = new ListElement();
+            newElement.setValue(value);
+            
+            setFirstElement(newElement);
+            setLastElement(newElement);
+        } else {
+            ListElement newElement = new ListElement();
+            newElement.setValue(value);
+            
+            getLastElement().setNextElement(newElement);
+        }
+    }
+    /*
+    noch nicht fertig \|/
+    */
+    public void add(int index, String value){
+        if(size++ >= index){
+            ListElement newElement = new ListElement();
+            ListElement ptrElement = new ListElement();
+            ptrElement = getFirstElement();
+            newElement.setValue(value);
+            
+            while(index-- > 0){
+                ptrElement = ptrElement.getNextElement();
+            }
+            
+            newElement = ptrElement.getNextElement();
+            ptrElement.setNextElement(newElement);
+            newElement.setNextElement(ptrElement.getNextElement());
+        } else {
+            
+        }
+    }
+    
+    public void removeFirstElement(){
+        
+    }
+    
+    public void removeLastElement(){
+        
+    }
+    
+    public void remove(int index){
         
     }
     
